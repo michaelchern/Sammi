8280f36a43b3b884806dd185a969ee97fdbc1ed6
michaelchern <1216866818@qq.com>
2025-07-05 11:08:18 +0800

    Update fxaa_pass.h

--- engine/source/runtime/function/render/passes/fxaa_pass.h
+++ engine/source/runtime/function/render/passes/fxaa_pass.h
@@ -1,8 +1,8 @@
-#pragma once
+﻿#pragma once
 
 #include "runtime/function/render/render_pass.h"
 
-namespace Piccolo
+namespace Sammi
 {
     class WindowUI;
 
@@ -25,4 +25,4 @@ namespace Piccolo
         void setupPipelines();
         void setupDescriptorSet();
     };
-} // namespace Piccolo
+}

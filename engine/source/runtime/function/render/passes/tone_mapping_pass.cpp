ac4f66883015c3290c7e21334ff56a05b35c6e0e
michaelchern <1216866818@qq.com>
2025-07-06 22:05:39 +0800

    Update tone_mapping_pass.cpp

--- engine/source/runtime/function/render/passes/tone_mapping_pass.cpp
+++ engine/source/runtime/function/render/passes/tone_mapping_pass.cpp
@@ -1,4 +1,4 @@
-#include "runtime/function/render/passes/tone_mapping_pass.h"
+﻿#include "runtime/function/render/passes/tone_mapping_pass.h"
 
 #include "runtime/function/render/interface/vulkan/vulkan_rhi.h"
 #include "runtime/function/render/interface/vulkan/vulkan_util.h"
@@ -8,7 +8,7 @@
 
 #include <stdexcept>
 
-namespace Piccolo
+namespace Sammi
 {
     void ToneMappingPass::initialize(const RenderPassInitInfo* init_info)
     {
@@ -244,4 +244,4 @@ namespace Piccolo
 
         m_rhi->popEvent(m_rhi->getCurrentCommandBuffer());
     }
-} // namespace Piccolo
+}

1eeef9029e5bee8985268f077933cc88abd5b2a2
michaelchern <1216866818@qq.com>
2025-07-07 16:23:10 +0800

    Update render_pipeline.h

--- engine/source/runtime/function/render/render_pipeline.h
+++ engine/source/runtime/function/render/render_pipeline.h
@@ -13,12 +13,10 @@ namespace Sammi
         virtual void initialize(RenderPipelineInitInfo init_info) override final;
 
         // 前向渲染实现（重写基类函数）
-        virtual void forwardRender(std::shared_ptr<RHI>                rhi,
-                                   std::shared_ptr<RenderResourceBase> render_resource) override final;
+        virtual void forwardRender(std::shared_ptr<RHI> rhi, std::shared_ptr<RenderResourceBase> render_resource) override final;
 
         // 延迟渲染实现（重写基类函数）
-        virtual void deferredRender(std::shared_ptr<RHI>                rhi,
-                                    std::shared_ptr<RenderResourceBase> render_resource) override final;
+        virtual void deferredRender(std::shared_ptr<RHI> rhi, std::shared_ptr<RenderResourceBase> render_resource) override final;
 
         // 在交换链重建后更新通道资源（窗口大小/分辨率改变时调用）
         void passUpdateAfterRecreateSwapchain();

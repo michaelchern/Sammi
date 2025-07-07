bb99af2e8d3ee18be183872288c0b1ebb0e6ecc2
michaelchern <1216866818@qq.com>
2025-07-07 19:42:47 +0800

    update

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -1,4 +1,4 @@
-# this file contains a list of tools that can be activated and downloaded on-demand each tool is
+﻿# this file contains a list of tools that can be activated and downloaded on-demand each tool is
 # enabled during configuration by passing an additional `-DUSE_<TOOL>=<VALUE>` argument to CMake
 
 # only activate tools for top level project
